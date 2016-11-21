f332e2eb09f1ef3dce455ebd0fa8e661df4bb14c
Liam Howe <Liam Howe>
2016-11-21 12:42:40 +0000

    updated starter kit

---
+++ ThomasWasAlone/Tile.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Tile.h"
+
+
+Tile::Tile(Point2D pos,float _width,float _height) 
+{
+	m_Pos = pos;
+	m_Size.w = _width;
+	m_Size.h = _height;
+	Rect *_Rect = new Rect(m_Pos.x, m_Pos.y, m_Size.w, m_Size.h);
+	m_Rect.push_back(*_Rect);
+}
+
+Tile::~Tile()
+{
+}
+
+void Tile::Render(Renderer& r) {
+	for (int i = 0; i < m_Rect.size(); i++)
+	{
+		r.drawWorldRect(m_Rect[i], Colour(0, 0, 0));
+	}
+}
+
+
+void Tile::Update(unsigned int deltaTime) {
+
+	//float angle = angVel*deltaTime / 1000;
+	//Point2D p = rect.pos;//current position
+
+	//					 //apply a rotation about the origin transform
+	//rect.pos.x = (float)cos(angle)*p.x - (float)sin(angle)*p.y;
+	//rect.pos.y = (float)sin(angle)*p.x + (float)cos(angle)*p.y;
+
+}
+
---
+++ ThomasWasAlone/Tile.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "BasicTypes.h"
+#include "GameObject.h"
+#include <vector>
+
+class Tile: public GameObject {
+
+public :
+
+	Tile(Point2D pos, float _width, float _height);
+	~Tile();
+	void Render(Renderer & r);
+	void Update(unsigned int deltaTime);
+
+
+private:
+	Point2D m_Pos;
+	Size2D m_Size;
+	std::vector<Rect> m_Rect;
+
+
+};
